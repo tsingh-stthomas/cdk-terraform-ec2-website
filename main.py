36c993bc7308ca7000da0b7198af10cb6690f9a1
EC2 Default User <ec2-user@ip-172-31-35-31.ec2.internal>
2024-03-24 02:11:18 +0000

    Adding code

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python
 from constructs import Construct
-from cdktf import App, TerraformStack
+from cdktf import App, TerraformStack, TerraformOutput
+from cdktf_cdktf_provider_aws.provider import AwsProvider
+from cdktf_cdktf_provider_aws.instance import Instance
 
+class CdkTerraformEc2WebsiteStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
 
-class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, id: str):
-        super().__init__(scope, id)
+        # Define AWS provider
+        AwsProvider(self, "Aws", region="us-east-1")
+        
 
-        # define resources here
+        # Define EC2 instance
+        instance = Instance(self, "WebServer",
+                                ami="ami-0c55b159cbfafe1f0",
+                                instance_type="t2.micro",
+                                tags={"Name": "WebServer"}
+                                )
 
+        # Output the public IP of the EC2 instance
+        TerraformOutput(
+            self,
+            "public_ip",
+            value=instance.public_ip
+        )
 
 app = App()
-MyStack(app, "cdk-terraform-ec2-website")
-
+CdkTerraformEc2WebsiteStack(app, "cdk-terraform-ec2-website")
 app.synth()
